ce572ae01a6117cda37fa8311233213fb6243116
Michael Alt <mail@michael-alt.info>
2021-03-03 22:20:58 +0100

    Add a simple sieve.

--- PrimesLibrary/Primes.cs
+++ PrimesLibrary/Primes.cs
@@ -4,14 +4,20 @@ namespace PrimesLibrary
 {
     public static class Primes
     {
-        public static bool IsPrimeNumber(ulong z)
+        public static bool IsPrimeNumber(ulong number)
         {
             // Handling egde cases
-            if (z < 2) return false;
-            if (z == 2) return true;
+            if (number < 2) return false;
+            if (number == 2) return true;
 
             // Handling even numbers
-            if (z % 2 == 0) return false;
+            if (number % 2 == 0) return false;
+
+            // And my sieve ...
+            for (ulong divisor = 3; divisor <= (ulong) Math.Sqrt(number); divisor += 2)
+            {
+                if (number % divisor == 0) return false;
+            }
 
             return true;
         }
--- PrimesLibraryTests/PrimesTests.cs
+++ PrimesLibraryTests/PrimesTests.cs
@@ -2,6 +2,7 @@
 using PrimesLibrary;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace PrimesLibrary.Tests
@@ -34,10 +35,20 @@ namespace PrimesLibrary.Tests
         public void IsEvenNumberAPrimeNumberTest()
         {
             Random rnd = new System.Random();
-            ulong evenNumber = (ulong) (rnd.Next() + 1) * 2;
+            ulong evenNumber = (ulong)(rnd.Next() + 1) * 2;
 
             bool isEvenNumberPrimeNumber = Primes.IsPrimeNumber(evenNumber);
             Assert.AreEqual(isEvenNumberPrimeNumber, false);
+        }
+
+        [TestMethod()]
+        public void WellKnownPrimeNumbersTest()
+        {
+            // Three non-primes: 25, 39 and 77
+            ulong[] numbers = new ulong[] { 5, 7, 11, 13, 19, 23, 25, 29, 39, 71, 77, 173 };
+
+            int primesCount = numbers.Select(z => Primes.IsPrimeNumber(z)).Where(isPrime => isPrime).Count();
+            Assert.AreEqual(primesCount, numbers.Length - 3);
         }
     }
 }
